d7b7558c0af151be5a82d866c9629ddead1ef63d
Plads <lumoximax2@gmail.com>
2025-11-17 22:23:43 -0500

    fix ruff errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -38,7 +38,7 @@ def si1_workflow(default_page: Page) -> Generator[Page]:
             }}
             """,
     )
-    yield default_page
+    return default_page
 
 
 @pytest.fixture
@@ -51,7 +51,7 @@ def si3_workflow(default_page: Page) -> Generator[Page]:
             }}
             """,
     )
-    yield default_page
+    return default_page
 
 
 # TODO:
@@ -70,4 +70,4 @@ def si4_workflow(default_page: Page) -> Generator[Page]:
             }}
             """,
     )
-    yield default_page
+    return default_page
--- tests/workflows/__init__.py
+++ tests/workflows/__init__.py
@@ -1,3 +1,4 @@
+# noqa: N999
 from .SI1 import SI1  # noqa: F401
 from .SI3 import SI3  # noqa: F401
 from .SI4 import SI4  # noqa: F401
