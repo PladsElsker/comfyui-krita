e4796f047a6b42008b00e409890cdf2411d4a7e8
JohnWL <34081873+John-WL@users.noreply.github.com>
2025-12-05 17:11:41 -0400

    attempt at making code more clean

--- pykrita/comfyui/extension/ui/docker/node_list.py
+++ pykrita/comfyui/extension/ui/docker/node_list.py
@@ -8,6 +8,9 @@ from comfyui.extension.ui.nodes.comfyui_node import ComfyUiNode
 from comfyui.extension.ui.nodes.node_factory import NodeFactory
 
 
+NodeKey = tuple[int, str, str | None]
+
+
 class NodeListWidget(QScrollArea):
     def __init__(self) -> None:
         super().__init__()
@@ -26,20 +29,42 @@ class NodeListWidget(QScrollArea):
         self.setWidget(self.container)
 
         self.node_widgets: list[ComfyUiNode] = []
-        self.prevous_node_states: dict[tuple[int, str, str], UiNodeState] = {}
+        self.previous_node_states: dict[NodeKey, UiNodeState] = {}
 
     def update_from_comyui(self, nodes: list[Node], window: Window, document: Document, document_id: str) -> None:
-        def _nid(node: Node) -> tuple:
-            return (node.id, node.type, document_id)
+        comfyui_node_mapping = {self._get_node_key(node, document_id): node for node in nodes}
+        current_node_mapping = {self._get_node_key(node_widget, document_id): node_widget for node_widget in self.node_widgets}
 
-        comfyui_node_mapping = {_nid(node): node for node in nodes}
-        current_node_mapping = {_nid(node_widget): node_widget for node_widget in self.node_widgets}
+        incoming_keys = comfyui_node_mapping.keys()
+        existing_keys = current_node_mapping.keys()
 
-        nodes_to_delete = [
-            current_node_mapping[_nid(node_widget)] for node_widget in self.node_widgets if _nid(node_widget) not in comfyui_node_mapping
-        ]
-        nodes_to_create = [node for node in nodes if _nid(node) not in current_node_mapping]
+        to_delete_keys = existing_keys - incoming_keys
+        to_create_keys = incoming_keys - existing_keys
 
+        self._clear_main_layout()
+
+        for to_delete_key in to_delete_keys:
+            widget: ComfyUiNode = current_node_mapping[to_delete_key]
+
+            if isinstance(widget, ComfyUiNode):
+                widget.cleanup()
+                widget.deleteLater()
+                self.node_widgets.remove(widget)
+
+        for to_create_key in to_create_keys:
+            to_create = comfyui_node_mapping[to_create_key]
+            node_widget = NodeFactory.create(self, to_create, window, document, document_id)
+
+            if to_create_key in self.previous_node_states:
+                previous_node_state = self.previous_node_states[to_create_key]
+                node_widget.apply(previous_node_state)
+
+            node_widget.state_changed.connect(self._register_node_state)
+            self.node_widgets.append(node_widget)
+
+        self._refresh_ui()
+
+    def _clear_main_layout(self) -> None:
         while self.main_layout.count() > 0:
             item = self.main_layout.takeAt(0)
 
@@ -49,28 +74,6 @@ class NodeListWidget(QScrollArea):
                 if layout_widget is not None and not isinstance(layout_widget, ComfyUiNode):
                     layout_widget.deleteLater()
 
-        for to_delete in nodes_to_delete:
-            node_id = _nid(to_delete)
-            widget: ComfyUiNode = current_node_mapping[node_id]
-
-            if isinstance(widget, ComfyUiNode):
-                widget.cleanup()
-                widget.deleteLater()
-                self.node_widgets.remove(widget)
-
-        for to_create in nodes_to_create:
-            node_widget = NodeFactory.create(self, to_create, window, document, document_id)
-            node_id = _nid(to_create)
-
-            if node_id in self.prevous_node_states:
-                previous_node_state = self.prevous_node_states[node_id]
-                node_widget.apply(previous_node_state)
-
-            node_widget.state_changed.connect(self._register_node_state)
-            self.node_widgets.append(node_widget)
-
-        self._refresh_ui()
-
     def _refresh_ui(self) -> None:
         input_mode: NodeDirection = "input"
         output_mode: NodeDirection = "output"
@@ -78,23 +81,19 @@ class NodeListWidget(QScrollArea):
         input_node_widgets = [node_widget for node_widget in self.node_widgets if node_widget.direction == input_mode]
         output_node_widgets = [node_widget for node_widget in self.node_widgets if node_widget.direction == output_mode]
 
-        if len(input_node_widgets) > 0:
-            self.main_layout.addWidget(GroupTitle("TO COMFYUI (INPUTS)"))
-
-        for i, node_widget in enumerate(input_node_widgets):
-            self.main_layout.addWidget(node_widget)
-            if i < len(input_node_widgets) - 1:
-                self._add_separator()
-
-        if len(output_node_widgets) > 0:
-            self.main_layout.addWidget(GroupTitle("FROM COMFYUI (OUTPUTS)"))
-
-        for i, node_widget in enumerate(output_node_widgets):
-            self.main_layout.addWidget(node_widget)
-            if i < len(output_node_widgets) - 1:
-                self._add_separator()
+        self._add_node_group(input_node_widgets, "TO COMFYUI (INPUTS)")
+        self._add_node_group(output_node_widgets, "FROM COMFYUI (OUTPUTS)")
 
         self.main_layout.addStretch(1)
+
+    def _add_node_group(self, nodes: list[ComfyUiNode], title: str) -> None:
+        if len(nodes) > 0:
+            self.main_layout.addWidget(GroupTitle(title))
+
+        for i, node_widget in enumerate(nodes):
+            self.main_layout.addWidget(node_widget)
+            if i < len(nodes) - 1:
+                self._add_separator()
 
     def _add_separator(self) -> None:
         line = QFrame()
@@ -105,7 +104,10 @@ class NodeListWidget(QScrollArea):
         self.main_layout.addWidget(line)
 
     def _register_node_state(self, state: UiNodeState) -> None:
-        self.prevous_node_states[(state.id, state.type, state.document_id)] = state
+        self.previous_node_states[self._get_node_key(state)] = state
+
+    def _get_node_key(self, node: Node | ComfyUiNode | UiNodeState, document_id: str | None = None) -> NodeKey:
+        return (node.id, node.type, document_id if document_id is not None else node.document_id)
 
 
 class GroupTitle(QLabel):
